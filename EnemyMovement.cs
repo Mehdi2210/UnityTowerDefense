d8ccfc5380dab8239cff84b4163e9bb7c382554c
MehdiEl <40770264+Mehdi2210@users.noreply.github.com>
2018-07-04 17:28:16 +0100

    Add files via upload
    The turn points script, is a script which specifies the turns the enemies have to take to follow the road, they are basically empty game object unseen by the player, and the EnemyMovement is a script which leads the enemy from a point to another

---
+++ EnemyMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    
+
+    void Start()
+    {
+
+        target = TurnPoints.points[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+         transform.Translate(dir.normalized *speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+
+        
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= TurnPoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointIndex++;
+        target = TurnPoints.points[wavepointIndex];
+    }
+
+   
+}
